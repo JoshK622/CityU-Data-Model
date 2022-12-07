ebd11a478c756bf20db5a53e28f04756f0cbb0b1
joshk622 <joshk0622>
2022-12-07 09:35:52 +0800

    Data uploaded

--- README.md
+++ README.md
@@ -8,3 +8,7 @@
  
 -  Run scrape_model.py after modifying the chromedriver directory
 -  The model is running in headless mode in default, comment line 22 to check the browser behaviour
+
+ Load data to MongoDB:
+
+- run cityU_import.py after modifying the DBname and collection name
---
+++ cityU_import.py
@@ -0,0 +1,16 @@
+import json
+from pymongo import MongoClient
+import os
+
+db_name = "ctuDB"
+collection_name = "ctu"
+client = MongoClient('localhost', 27017)
+db = client[db_name]
+collection = db[collection_name]
+filenames = os.listdir(
+    './data/')
+print(filenames)
+for filename in filenames:
+    os.system(
+            'mongoimport --host localhost --port 27017 --collection ' + collection_name+ '--db ' + db_name + ' --jsonArray ./data/'+filename) 
+client.close()
